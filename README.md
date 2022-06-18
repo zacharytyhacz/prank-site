95ed220d3eab1fad8b97dce67a421c1bda166f5f
zacharytyhacz <zactyh@gmail.com>
2022-06-17 23:54:29 -0400

    ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷V

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Just a prank site to make it look like you're getting hacked.
 - [x] Plays an obnoxious video in background
 - [x] Prompts you before closing window
 - [x] Prompts a download for TempleOS.ISO
-- [ ] Random download gifs appear
-- [ ] Hide cursor
+- [x] Random download gifs appear
+- [x] Hide cursor
 - [ ] Random typing of text on screen ( Terry Davis quotes )
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,8 +2,20 @@ window.onload = function(){
   document.getElementsByTagName('video')[0].onplay = function() {
 
     document.getElementsByTagName('video')[0].style.opacity = 1
-    document.querySelector('.get-started').style.display = 'none'
-    document.getElementsByTagName('body')[0].style.cursor = 'text !important'
+
+    document.querySelector('.get-started').style.pointerEvents = 'initial'
+    document.querySelector('.get-started').style.width = '50vw'
+    document.querySelector('.get-started').style.height = '1vh'
+    document.querySelector('.get-started').style.zIndex = 9999999
+    document.querySelector('.get-started').style.cursor = 'col-resize'
+    document.querySelector('.get-started').style.position = 'relative'
+
+    const images = document.querySelectorAll('img')
+    for (let i = 0; i < images.length; i++) {
+      images[i].style.display = 'block'
+    }
+
+    document.getElementsByTagName('body')[0].style.cursor = 'none'
 
     window.onbeforeunload = function(){
       return 'I was chosen by God because I am the best programmer on the planet and God boosted my IQ with divine intellect.';
@@ -18,6 +30,7 @@ window.onload = function(){
 
     setInterval(function() {
       const faviconLink = document.querySelector('#favicon')
+      console.log('here', faviconLink.href)
 
       if (faviconLink.href !== '/images/alert1.png') {
         faviconLink.href = '/images/alert1.png'
